40ccb4555d8bdc7915dfa78dec717e298c42cbe5
ZhuangEr <huangyu.wuhan@gmail.com>
2016-09-29 17:50:56 -0400

    implement command spout and order spout

---
+++ src/main/storm/spout/CommandSpout.java
@@ -0,0 +1,49 @@
+package storm.spout;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+
+import storm.config.FieldNames;
+
+public class CommandSpout extends BaseRichSpout {
+
+	private static final long serialVersionUID = 3757047085011759927L;
+
+	public static Logger LOG = Logger.getLogger(CommandSpout.class);
+	
+	private SpoutOutputCollector collector;
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void open(Map conf, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+		this.collector = spoutOutputCollector;
+	}
+
+	@Override
+	public void nextTuple() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.err.println("****************---------------------=====================");
+		collector.emit(new Values("statistic"));
+	}
+
+	@Override
+	public void declareOutputFields(
+			OutputFieldsDeclarer outputFieldsDeclarer) {
+		outputFieldsDeclarer.declare(new Fields(
+				FieldNames.COMMAND
+		));
+	}
+
+}
